8b7085776e4ef03a9f0d6fa60a916eae37110727
Yu Fang <157097349+ShiraishiHo@users.noreply.github.com>
2026-03-26 10:18:46 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,41 @@
+const CACHE_NAME = 'academic-timer-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://fonts.cdnfonts.com/css/digital-7-mono'
+];
 
+// 安装阶段：将文件存入缓存
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// 激活阶段：清理旧缓存
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 拦截请求：优先从缓存读取
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
