c0780470ab62279c8caaf9ee70b6469b2680339b
Yu Fang <157097349+ShiraishiHo@users.noreply.github.com>
2026-03-26 10:43:37 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
-const CACHE_NAME = 'academic-timer-v1';
+const CACHE_NAME = 'academic-timer-v3';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  'https://fonts.cdnfonts.com/css/digital-7-mono'
+  'https://fonts.cdnfonts.com/css/digital-7-mono',
+  'https://images.unsplash.com/photo-1508962914676-134849a727f0?q=80&w=180&h=180&auto=format&fit=crop',
+  'https://images.unsplash.com/photo-1508962914676-134849a727f0?q=80&w=192&h=192&auto=format&fit=crop',
+  'https://images.unsplash.com/photo-1508962914676-134849a727f0?q=80&w=512&h=512&auto=format&fit=crop'
 ];
 
 // 安装阶段：将文件存入缓存
